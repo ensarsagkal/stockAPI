1fdcb2a64a3767e58d99f56747d285703abc0aec
ensar <ensarsagkal@gmail.com>
2024-04-15 20:15:20 +0300

    up

--- src/models/user.js
+++ src/models/user.js
@@ -5,9 +5,31 @@
 const { mongoose } = require('../configs/dbConnection')
 /* ------------------------------------------------------- *
 {
+    "username": "admin",
+    "password": "aA*123456",
+    "email": "admin@site.com",
+    "firstName": "admin",
+    "lastName": "admin",
+    "isActive": true,
+    "isStaff": true,
+    "isAdmin": true
+}
+{
+    "username": "staff",
+    "password": "aA*123456",
+    "email": "staff@site.com",
+    "firstName": "staff",
+    "lastName": "staff",
+    "isActive": true,
+    "isStaff": true,
+    "isAdmin": false
+}
+{
     "username": "test",
-    "password": "1234",
+    "password": "aA*123456",
     "email": "test@site.com",
+    "firstName": "test",
+    "lastName": "test",
     "isActive": true,
     "isStaff": false,
     "isAdmin": false
