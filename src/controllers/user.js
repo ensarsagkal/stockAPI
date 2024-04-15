1751dd657255fd6adfdb4c0bafc4b106820b8c45
ensar <ensarsagkal@gmail.com>
2024-04-15 21:32:43 +0300

    up

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -77,15 +77,82 @@ module.exports = {
     },
 
     read: async (req, res) => {
+        /*
+            #swagger.tags = ["Users"]
+            #swagger.summary = "Get Single User"
+        */
+
+        // Sadece kendi kaydını görebilir:
+        const customFilters = req.user?.isAdmin ? { _id: req.params.id } : { _id: req.user._id }
+
+        // const data = await User.findOne({ _id: req.params.id })
+        const data = await User.findOne(customFilters)
+
+        res.status(200).send({
+            error: false,
+            data
+        })
 
     },
 
     update: async (req, res) => {
+        /*
+            #swagger.tags = ["Users"]
+            #swagger.summary = "Update User"
+            #swagger.parameters['body'] = {
+                in: 'body',
+                required: true,
+                schema: {
+                    "username": "test",
+                    "password": "1234",
+                    "email": "test@site.com",
+                    "firstName": "test",
+                    "lastName": "test",
+                }
+            }
+        */
+
+        // Sadece kendi kaydını güncelleyebilir:
+        const customFilters = req.user?.isAdmin ? { _id: req.params.id } : { _id: req.user._id }
+
+        // Yeni kayıtlarda admin/staff durumunu değiştiremez:
+        if (!req.user?.isAdmin) {
+            delete req.body.isStaff
+            delete req.body.isAdmin
+        }
+        
+        const data = await User.updateOne(customFilters, req.body, { runValidators: true })
+
+        res.status(202).send({
+            error: false,
+            data,
+            new: await User.findOne(customFilters),
+        })
 
     },
 
     delete: async (req, res) => {
+        /*
+            #swagger.tags = ["Users"]
+            #swagger.summary = "Delete User"
+        */
 
+        // Permission tarafında permissions.isAdmin kontrolü yapıldığı için burda gerek kalmadı.
+
+        if (req.params.id != req.user._id) {
+
+            const data = await User.deleteOne({ _id: req.params.id })
+    
+            res.status(data.deletedCount ? 204 : 404).send({
+                error: !data.deletedCount,
+                data
+            })
+
+        } else {
+            // Admin kendini silemez.
+            res.errorStatusCode = 403
+            throw new Error('You can not remove your account.')
+        }
     },
 
 }
