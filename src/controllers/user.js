d963d7f21496c571bcdded9332000b938b4330e6
ensar <ensarsagkal@gmail.com>
2024-04-15 20:30:41 +0300

    up

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -2,3 +2,54 @@
 /* -------------------------------------------------------
     NODEJS EXPRESS | CLARUSWAY FullStack Team
 ------------------------------------------------------- */
+// User Controller:
+
+const User = require('../models/user')
+
+module.exports = {
+
+    list: async (req, res) => {
+        /*
+            #swagger.tags = ["Users"]
+            #swagger.summary = "List Users"
+            #swagger.description = `
+                You can use <u>filter[] & search[] & sort[] & page & limit</u> queries with endpoint.
+                <ul> Examples:
+                    <li>URL/?<b>filter[field1]=value1&filter[field2]=value2</b></li>
+                    <li>URL/?<b>search[field1]=value1&search[field2]=value2</b></li>
+                    <li>URL/?<b>sort[field1]=asc&sort[field2]=desc</b></li>
+                    <li>URL/?<b>limit=10&page=1</b></li>
+                </ul>
+            `
+        */
+
+        // Sadece kendi kayıtlarını görebilir:
+        const customFilters = req.user?.isAdmin ? {} : { _id: req.user._id }
+
+        const data = await res.getModelList(User, customFilters)
+
+        res.status(200).send({
+            error: false,
+            details: await res.getModelListDetails(User),
+            data
+        })
+
+    },
+
+    create: async (req, res) => {
+
+    },
+
+    read: async (req, res) => {
+
+    },
+
+    update: async (req, res) => {
+
+    },
+
+    delete: async (req, res) => {
+
+    },
+
+}
--- src/models/user.js
+++ src/models/user.js
@@ -35,7 +35,7 @@ const { mongoose } = require('../configs/dbConnection')
     "isAdmin": false
 }
 /* ------------------------------------------------------- */
-
+// User Model
 const UserSchema = new mongoose.Schema({
 
     username: {
