defb1d228227ffa9d5d71f1b12d613d641ee7a29
ensar <ensarsagkal@gmail.com>
2024-04-18 15:26:27 +0300

    up

--- src/routes/product.js
+++ src/routes/product.js
@@ -4,3 +4,19 @@
 ------------------------------------------------------- */
 const router = require('express').Router()
 /* ------------------------------------------------------- */
+// routes/product:
+
+const product = require('../controllers/product')
+const permissions = require('../middlewares/permissions')
+
+// URL: /products
+
+router.route('/(:id)?')
+    .post(permissions.isAdmin, product.create)
+    .get(permissions.isStaff, product.read)
+    .put(permissions.isAdmin, product.update)
+    .patch(permissions.isAdmin, product.update)
+    .delete(permissions.isAdmin, product.delete)
+
+/* ------------------------------------------------------- */
+module.exports = router
