56f90a0fd3265ea43ab277a7371e34774bd66585
ensar <ensarsagkal@gmail.com>
2024-04-18 12:31:47 +0300

    up

--- src/routes/index.js
+++ src/routes/index.js
@@ -16,18 +16,18 @@ router.use('/users', require('./user'))
 router.use('/tokens', require('./token'))
 
 
-// // brand:
-// router.use('/brands', require('./brand'))
-// // category:
-// router.use('/categories', require('./category'))
-// // firm:
-// router.use('/firms', require('./firm'))
-// // product:
-// router.use('/products', require('./product'))
-// // purchase:
-// router.use('/purchases', require('./purchase'))
-// // sale:
-// router.use('/sales', require('./sale'))
+// brand:
+router.use('/brands', require('./brand'))
+// category:
+router.use('/categories', require('./category'))
+// firm:
+router.use('/firms', require('./firm'))
+// product:
+router.use('/products', require('./product'))
+// purchase:
+router.use('/purchases', require('./purchase'))
+// sale:
+router.use('/sales', require('./sale'))
 
 // document:
 router.use('/documents', require('./document'))
