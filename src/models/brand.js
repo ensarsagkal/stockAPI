aa5d1714c4f2101237f0a0bf93a3682201a2e7d8
ensar <ensarsagkal@gmail.com>
2024-04-18 15:13:00 +0300

    up

--- src/models/brand.js
+++ src/models/brand.js
@@ -4,3 +4,26 @@
 ------------------------------------------------------- */
 const { mongoose } = require('../configs/dbConnection')
 /* ------------------------------------------------------- */
+// Brand Model:
+
+const BrandSchema = new mongoose.Schema({
+
+    name: {
+        type: String,
+        trim: true,
+        required: true,
+        unique: true
+    },
+
+    image: {
+        type: String,
+        trim: true,
+    }
+
+}, {
+    collection: 'brands',
+    timestamps: true
+})
+
+/* ------------------------------------------------------- */
+module.exports = mongoose.model('Brand', BrandSchema)
